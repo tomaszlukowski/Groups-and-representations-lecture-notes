5dd0c2c9adbed727953d6b07dbaa5b69dc39c747
tomaszlukowski <t.lukowski@herts.ac.uk>
2021-12-29 11:52:44 +0000

    Add first three lectures

---
+++ _build/jupyter_execute/Lectures/Lecture1.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.core.display import HTML as Center
+
+Center(""" <style>
+.output_png {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+}
+</style> """)
+
+
+# # Lecture 1 - Introduction
+# 
+# ## Symmetries
+# 
+# **Symmetries** provide one of the key ideas in modern mathematics and physics. They are naturally associated with the notion of **groups**. A typical problem in this context is to construct a system that behaves in a certain way under the action of a symmetry, for example, a system that is invariant under a given symmetry. 
+# 
+# To tackle this kind of problems, we need to know how the symmetries act on the building blocks of systems and these blocks are often the elements of vector spaces, i.e. vectors. This leads to a natural question of how to classify such actions of groups on vector spaces. The mathematical theory that deals with this question is the **representation theory**.
+# 
+# $$
+# \begin{array}{cc}
+# \mathrm{Physics}&&\mathrm{Mathematics}\\
+# \mathrm{symmetry}&\leftrightarrow&\mathrm{group}
+# \end{array}
+# $$
+# 
+# Many systems in classical and quantum physics are invariant under some symmetry operations. We distinguish two types of symmetries: **discrete** and **continuous** symmetries. 
+# 
+# In the first cathegory we have for example
+# * reflection (parity) symmetry
+# * discrete rotations
+# * time reversal
+# * CPT symmetry of the Standard Model
+# 
+# Continuous symmetries include:
+# * time translations
+# * space translations
+# * spatial rotations
+# * Poincare transformations
+# * gauge symmetries of quuntum field theories
+# 
+# Each symmetry can be described mathematically using groups and their representations.
+# 
+# ## First look at groups and representations
+# 
+# Consider the following square in 2D space:
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
+
+pts = np.array([[0,1], [1,0], [0,-1],[-1,0]])
+p = Polygon(pts, closed=True)
+ax = plt.gca()
+ax.set_aspect('equal', 'box')
+ax.grid()
+ax.add_patch(p)
+ax.set_xlim(-1.5,1.5)
+ax.set_ylim(-1.5,1.5)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.annotate("$x_2=(0,1)$", xy=(0, 1), xytext=(0.25, 1.25),
+            arrowprops=dict(arrowstyle="->"))
+ax.annotate("$x_1=(1,0)$", xy=(1,0), xytext=(1.25, 0.25),
+            arrowprops=dict(arrowstyle="->"))
+ax.annotate("$x_3=(-1,0)$", xy=(-1,0), xytext=(-1.5, -0.5),
+            arrowprops=dict(arrowstyle="->"))
+ax.annotate("$x_4=(0,-1)$", xy=(0,-1), xytext=(0.25, -1.25),
+            arrowprops=dict(arrowstyle="->"))
+plt.show()
+
+
+# It is symmetric under rotations and reflections. For example: the rotation by $90^\circ$ clockwise does not change the shape of the square. However, it **permutes** its vertices in the following way:
+# 
+# $$x_1\to x_4\to x_3 \to x_2 \to x_1$$
+# 
+# We associate a *permutation* to this symmetry: $(1432)$.
+# 
+# Another example: lreflection with respect to the $x$-axis. It permutes vertices:
+# 
+# $$x_2 \to x_4 \to x_2,$$
+# 
+# and $x_1$ and $x_3$ are unchanged.
+# We associate another permutation to this symmetry: $(1)(3)(24)=(24)$.
+# 
+# There are 8 such symmetries in total (four rotations and four reflections):
+# * $e_1=id$ - identity
+# * $e_2=(1432)$ - rotation by $90^\circ$ clockwise
+# * $e_3=(13)(24)$ - rotation by $180^\circ$ clockwise
+# * $e_4=(1234)$ - rotation by $270^\circ$ clockwise
+# * $e_5=(24)$ - $x$-axis reflection 
+# * $e_6=(13)$ - $y$-axis reflection
+# * $e_7=(14)(23)$ - reflection with respect to the top-left to bottom-right diagonal
+# * $e_8=(12)(34)$ - reflection with respect to the top-right to bottom-left diagonal
+# 
+# Two symmetries applied one after another is also a symmetry: $e_2$ followed by $e_2$ is $e_3$. We indicate it as 
+# 
+# $$e_2 \star e_2 =e_3$$
+# 
+# where $\star$ is a multiplication of group elements (binary operation). This can be done using vertex permutations:
+# 
+# $$(1432)\circ(1432) =(13)(24)$$
+# 
+# where we used the symbol $\circ$ to indicate multiplication (composition) of permutations.
+# 
+# One can construct a table collecting all results of multiplications of two symmetries - **Cayley table**.
+# 
+# This group is called the **dihedral group $D_4$**
+# 
+# $$
+# \begin{array}{c||c|c|c|c|c|c|c|c}
+# \star&e_1&e_2&e_3&e_4&e_5&e_6&e_7&e_8\\
+# \hline
+# \hline
+# e_1&e_1&e_2&e_3&e_4&e_5&e_6&e_7&e_8\\
+# \hline
+# e_2&e_2&e_3&e_4&e_1&e_7&e_8&e_6&e_5\\
+# \hline
+# e_3&e_3&e_4&e_1&e_2&e_6&e_5&e_8&e_7\\
+# \hline
+# e_4&e_4&e_1&e_2&e_3&e_8&e_7&e_5&e_6\\
+# \hline
+# e_5&e_5&e_8&e_6&e_7&e_1&e_3&e_4&e_2\\
+# \hline
+# e_6&e_6&e_7&e_5&e_8&e_3&e_1&e_2&e_4\\
+# \hline
+# e_7&e_7&e_5&e_8&e_6&e_2&e_4&e_1&e_3\\
+# \hline
+# e_8&e_8&e_6&e_7&e_5&e_4&e_2&e_3&e_1
+# \end{array}
+# $$
+# 
+# Some important observations from this table:
+# * each element appears only once in each column and in each row
+# * the element $e_1$ acts as the identity element, namely
+# 
+# $$e_1 \star e_i=e_i\star e_1=e_i$$
+# 
+# * every element has its inverse, i.e. for each $e_i$ there exists $e_j$ such that
+# 
+# $$e_i\star e_j=e_j\star e_i=e_1$$
+# 
+# These are common properties for all groups.
+# 
+# Groups are very abstract mathematical objects that are defined as a set with binary operation of its elements satisfying a series of conditions. We are interested in representing these elements in such a way that we can see how group elements act on the square. We can do it by associating $2\times 2$ matrices to each group element:
+# 
+# $$
+# \begin{align*}
+# &e_1 \to \begin{pmatrix}1&0\\0&1\end{pmatrix}\qquad & e_2 \to \begin{pmatrix}0&1\\-1&0\end{pmatrix}\qquad
+# &e_3 \to \begin{pmatrix}-1&0\\0&-1\end{pmatrix}\qquad & e_4 \to \begin{pmatrix}0&-1\\1&0\end{pmatrix}\\
+# &e_5 \to \begin{pmatrix}1&0\\0&-1\end{pmatrix}\qquad & e_6 \to \begin{pmatrix}1&0\\0&-1\end{pmatrix}\qquad
+# &e_7 \to \begin{pmatrix}0&-1\\-1&0\end{pmatrix}\qquad & e_8 \to \begin{pmatrix}0&1\\1&0\end{pmatrix}\\
+# \end{align*}
+# $$
+# 
+# These matrices satisfy the same multiplication table as the group elements. We call these set of matrices a representation of group $D_4$. This is not the only representation and we will discuss many more of them in this module.
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/Lectures/Lecture2.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Lecture 2 - Groups
+# 
+# ## Definition of a group
+# 
+# We start our adventure with groups and representations by defining what we mean by a group. There are two main ingredients in this definition: a group is a set with a (binary) operation defined on it. This means if we take two elements of the set, we can 'multiply' them and obtain another element of the same set. Not all sets with binary operations define groups, the operation needs to satisfy additional conditions. In particular, we often want to define more then two elements, and therefore we need to define what we mean by this, for example introducing appropriate brackets, such that at any stage we multiply only two elements at the time. For a binary operation to be a group multiplication, we demand that the operation is associative, which means that the result of multiplying more than two things does not depend on the way in which we put the brackets. Moreover, we demand that there is an identity element of the binary operation, which is an element that does not change the value of other elements when multiplied. Finally, every element should have an inverse element. A formal definition is given below:
+# 
+# ```{admonition} Definition (Group)
+# :class: definition
+# 
+# A **group** $(G,\star)$ is a set $G$ tohether with a binary operation
+# 
+# $$ \star:G\times G\to G$$
+# 
+# satisfying:
+# - <u>*Closure*</u>: For all $a,b\in G$, the result of the operation $a\star b$ is also in $G$
+# 
+# - <u>*Associativity*</u>: For all $a,b,c\in G$
+# 
+# $$a\star (b\star c)=(a \star b)\star c$$
+# 
+# - <u>*Identity element*</u>: There exists an element $e\in G$ such that for every element $a\in G$ the following holds:
+# 
+# $$e\star a=a\star e=a$$
+# 
+# Such element is unique and it is called the *identity element*.
+# 
+# - <u>*Inverse element*</u>: For each $a\in G$ there exists an element $b\in G$, commonly denoted $a^{-1}$, such that
+# 
+# $$a\star b=b\star a=e$$
+# 
+# ```
+# 
+# In the definition of a group we do not assume that the binary operation $\star$ is commutative, and it is important to realise that there are many groups that are not commutative. It is, however, useful to define a special class of groups, called *Abelian groups*, for which the order in which we multiply elements is not important.
+# 
+# ```{admonition} Definition (Abelian group)
+# :class: definition
+# 
+# A group $G$ is called an **Abelian group** if in addition to the conditions above it satisfies:
+# 
+# - <u>*Commutativity*</u>: For all $a,b\in G$ we have
+# 
+# $$a\star b=b\star a$$
+# ```
+# 
+# There are many examples of groups that you already know from your previous studies. We give some examples of the ones that might be new to you below.
+# 
+# 
+# ```{admonition} Examples (Group)
+# :class: example
+# 
+# - $(\mathbb{Z},+)$ - integer numbers with addition (identity element: $e=0$, inverse element: $a^{-1}=-a$, it is *Abelian*)
+# 
+# - $(\mathbb{R},+)$ - real numbers with addition (identity element: $e=0$, inverse element: $a^{-1}=-a$, it is *Abelian*)
+# 
+# - $(\mathbb{R}^+,\cdot)$ - poitive real numbers with multiplication (identity element: $e=1$, inverse element: $a^{-1}=\frac{1}{a}$, it is *Abelian*)
+# 
+# - *Symmetric group*: Let $X$ be any non-empty set. A **permutation** on $X$ is a function $f:X\to X$ such that $f$ is one-to-one. If $X$ is a finite set with $n$ elements then we call the set of all permutations on $X$ with composition, a symmetric group $S_n$.
+# 
+# For $n=3$, if $X=\{1,2,3\}$ then we have six permutations on $X$:
+# 
+# $$
+# \begin{align*}
+# &e=\left(\begin{matrix}1&2&3\\1&2&3\end{matrix}\right)\qquad &(12)=\left(\begin{matrix}1&2&3\\2&1&3\end{matrix}\right)\qquad
+# &(13)=\left(\begin{matrix}1&2&3\\3&2&1\end{matrix}\right)\\
+# &(23)=\left(\begin{matrix}1&2&3\\1&3&2\end{matrix}\right)\qquad &(123)=\left(\begin{matrix}1&2&3\\2&3&1\end{matrix}\right)\qquad
+# &(132)=\left(\begin{matrix}1&2&3\\3&1&2\end{matrix}\right)
+# \end{align*}
+# $$
+# 
+# The symmetric group $S_n$ for $n\leq 3$ is not Abelian, for example
+# 
+# $$(132)\circ(12)=\left(\begin{matrix}1&2&3\\3&1&2\end{matrix}\right)\circ\left(\begin{matrix}1&2&3\\2&1&3\end{matrix}\right)=\left(\begin{matrix}1&2&3\\1&3&2\end{matrix}\right)=(23)$$
+# 
+# $$(12)\circ(132)=\left(\begin{matrix}1&2&3\\2&1&3\end{matrix}\right)\circ \left(\begin{matrix}1&2&3\\3&1&2\end{matrix}\right)=\left(\begin{matrix}1&2&3\\3&2&1\end{matrix}\right)=(13)$$
+# 
+# and therefore $(132)\circ(12)\neq(12)\circ(132)$.
+# 
+# - *Alternating group*: if the permutation $f$ on the set $X$ interchanges just two elements of $X$, leaving all the other fixed, then $f$ is called a **transposition**. Every permutation can be expressed as a product of transpositions. We say that a permutation $f$ is even (odd) if $f$ can be expressed as a product of an even (odd) number of transpositions. The set of all even permutations on $X$ forms a group called the alternating group $A_n$.
+# 
+# For $n=3$:
+# 
+# $$
+# \begin{align*}
+# &e - \mathrm{zero \,\,transpositions}\\
+# &(12), (13),(23) - \mathrm{one \,\,transposition}\\
+# &(123)=(13)\circ (12), (132)=(13)\circ(23) - \mathrm{two\,\, transpositions}
+# \end{align*}
+# $$
+# 
+# Then $A_3=\{e,(123),(132)\}$.
+# 
+# - *Cyclic group*: for any $n$ consider the set $\mathbb{Z}_n=\{0,1,2,\ldots,n-1\}$ and define a binary operation $+\,(\textrm{mod}\,\, n)$ which is defined as: to compute $a+b\,(\mathrm{mod}\,\,n)$, add the integers $a$ and $b$, divide the result by $n$, and take the remainder. Then $(\mathbb{Z}_n,+\,(\mathrm{mod}\,\,n))$ is a group. Cyclic groups are abelian
+# 
+# For example, $\mathbb{Z}_4=\{0,1,2,3\}$ with the following multiplication table
+# 
+# $$
+# \begin{array}{c|c|c|c|c}
+# +\,(\mathrm{mod}\,\, 4)&0&1&2&3\\
+# \hline
+# 0&0&1&2&3\\
+# \hline
+# 1&1&2&3&0\\
+# \hline
+# 2&2&3&0&1\\
+# \hline
+# 3&3&0&1&2
+# \end{array}
+# $$
+# ```
+# 
+# ## Subgroups
+# 
+# Some of the subsets of groups are themselves groups.
+# 
+# ```{admonition} Definition (Subgroup)
+# :class: definition
+# 
+# Given a group $(G,\star)$, a subset $H$ of $G$ is called a **subgroup** of $G$, if $H$ also forms a group under the operation $\star$. We denote it by $H\leq G$.
+# ```
+# 
+# ```{admonition} Proposition
+# :class: proposition
+# 
+# A subset $H$ of a group $(G,\star)$ is a subgroup if $H$ is closed under $\star$ and it contains inverses.
+# ```
+# 
+# ```{admonition} Examples (Subgroup)
+# :class: example
+# 
+# - For every group $G$, the *trivial group* $\{e\}$ and the full group $G$ are subgroups of $G$.
+# 
+# - The set containing all even numbers $2\mathbb{Z}=\{2n:n\in \mathbb{Z}\}$ with addition is a subgroup of $(\mathbb{Z},+)$
+# 
+# - $(\{0,2\},+\,(\mathrm{mod}\,\,4))$ is a subgroup of $(\mathbb{Z}_4,+\,(\mathrm{mod}\,\,4))$ 
+# 
+# - $\{e,(12)\}$ is a subgroup of $S_3$
+# 
+# - *Center of a group*: the set
+# 
+# $$
+# Z(G)=\{x\in G:g\star x=x\star g \mbox{ for all }g\in G\}
+# $$
+# 
+# is a subgroup of $G$
+# ```
+# 
+# ## Order of an element
+# 
+# ```{admonition} Definition (Order of an element)
+# :class: definition
+# 
+# The **order of an element** $g\in G$ is the smallest positive integer $m$ such that
+# 
+# $$
+# a^m=e
+# $$
+# 
+# We denote the order of $g$ by $|g|$.
+# ```
+# 
+# 
+# ```{admonition} Example (Order of an element)
+# :class: example
+# 
+# - In $S_3$ the element $(12)$ has order 2 since $(12)\circ(12)=e$. Similarly, the element $(123)$ has order 3 since $(123)\circ(123)\circ(123)=e$.
+# 
+# - In $\mathbb{Z}$ all elements apart from 0 have infinite order since there is no $m$ such that $a+a+\ldots+a=0$ ($m$ times) for any $a\in \mathbb{Z}\setminus \{0\}$.
+# 
+# ```
+# 
+# ## Order of a group
+# 
+# ```{admonition} Definition (Order of a group)
+# :class: definition
+# 
+# The **order of a group** $G$ is its cardinality. We denote the order of $G$ by $|G|$.
+# 
+# ```
+# 
+# ```{admonition} Examples (Order of a group)
+# :class: example
+# 
+# - $(\mathbb{Z},+)$ has an infinite order
+# - the cyclic group group $(\mathbb{Z}_n,+\,\mathrm{mod}\,\, n)$ has order $n$
+# - the symmetric group $S_n$ has order $n!$, while the alternating group $A_n$ has order $\frac{n!}{2}$
+# 
+# ```
+# 
+# ## Subgroups generated by elements
+# 
+# ```{admonition} Definition (Subgroup generated by an element)
+# :class: definition
+# 
+# Let $g\in G$. Then the **subgroup generated by $g$** is
+# 
+# $$
+# \langle g\rangle= \{g^k:k\in \mathbb{Z}\}
+# $$
+# 
+# ```
+# 
+# ```{admonition} Example (Subgroup generated by an element)
+# :class: example
+# 
+# - In $S_3$: $\langle (123) \rangle=\{(123),(123)^2=(132),(123)^3=e\}$
+# 
+# - In $S_4$: $\langle(12)(34)\rangle=\{(12)(34),((12)(34))^2=e\}$
+# 
+# ```
+# 
+# ```{admonition} Proposition
+# :class: proposition
+# 
+# Let $g\in G$ and $H=\langle g\rangle$. Then $|H|=|g|$.
+# 
+# ```
+# 
+# ```{admonition} Definition (Subgroup generated by a set)
+# :class: definition
+# 
+# Let $S$ be a subset containing elements from a group $G$. Then the **subgroup generated by $S$** is
+# 
+# $$
+# \langle S\rangle=\{s_1^{k_1}\star s_2^{k_2} \star \ldots \star s_l^{k_l}:l\in\mathbb{N},s_i\in S,k_i\in \mathbb{Z}\}
+# $$
+# ```
+# 
+# ```{admonition} Example (Subgroup generated by a set)
+# :class: example
+# 
+# - In $S_4$: the subgroup generated by the set containing $(12)$ and $(34)$ is: $\langle \{(12),(34)\} \rangle=\{e,(12),(34),(12)(34)\}$. This subgroup is not cyclic, namely it cannot be generated by a single element in $S_4$.
+# 
+# ```
+
+# In[ ]:
+
+
+
+
